ec79bbd07a915f2478f8674d02031724099d1e78
Rafael Araujo <rafa123839@gmail.com>
2022-09-24 08:11:48 -0300

    perfil de concluido

---
+++ src/main/java/com/rafael/helpdesk/services/DBService.java
@@ -0,0 +1,45 @@
+package com.rafael.helpdesk.services;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.rafael.helpdesk.domain.Chamado;
+import com.rafael.helpdesk.domain.Cliente;
+import com.rafael.helpdesk.domain.Tecnico;
+import com.rafael.helpdesk.domain.enums.Perfil;
+import com.rafael.helpdesk.domain.enums.Prioridade;
+import com.rafael.helpdesk.domain.enums.Status;
+import com.rafael.helpdesk.repositories.ChamadoRepository;
+import com.rafael.helpdesk.repositories.ClienteRepository;
+import com.rafael.helpdesk.repositories.TecnicoRepository;
+
+@Service
+public class DBService {
+	
+	@Autowired
+	private TecnicoRepository tecnicoRepository;
+	
+//	@Autowired
+//	private PessoaRepository pessoaRepository;
+	
+	@Autowired
+	private ClienteRepository clienteRepository;
+	
+	@Autowired
+	private ChamadoRepository chamadoRepository; 
+	
+	public void instanciaDbB() {
+		Tecnico tec1 = new Tecnico(null, "Valdir cezar", "06539588380", "email@gmail.com", "123456");
+		tec1.addPerfil(Perfil.ADMIN);
+		
+		Cliente cli1 = new Cliente(null, "Linux Tovars", "7894562146", "linus@gmail.com", "7894654");
+		
+		Chamado c1 = new Chamado(null, Prioridade.MEDIA, Status.ANDAMENTO, "Chamdo 01", "Primeiro chamado", tec1, cli1);
+		
+		tecnicoRepository.saveAll(Arrays.asList(tec1));
+		clienteRepository.saveAll(Arrays.asList(cli1));
+		chamadoRepository.saveAll(Arrays.asList(c1));
+	}
+}
