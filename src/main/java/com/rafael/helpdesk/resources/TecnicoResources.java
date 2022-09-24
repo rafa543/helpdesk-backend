6c17222c9bde0bfbec695b08cee6be3ebd5f913b
Rafael Araujo <rafa123839@gmail.com>
2022-09-24 09:01:03 -0300

    padrao dto para tecnico

--- src/main/java/com/rafael/helpdesk/resources/TecnicoResources.java
+++ src/main/java/com/rafael/helpdesk/resources/TecnicoResources.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.rafael.helpdesk.domain.Tecnico;
+import com.rafael.helpdesk.domain.dtos.TecnicoDTO;
 import com.rafael.helpdesk.services.TecnicoService;
 
 @RestController
@@ -18,8 +19,8 @@ public class TecnicoResources {
 	private TecnicoService tecnicoService;
 	
 	@GetMapping(value="/{id}")
-	public ResponseEntity<Tecnico> findById(@PathVariable Integer id) {
+	public ResponseEntity<TecnicoDTO> findById(@PathVariable Integer id) {
 		Tecnico obj = tecnicoService.findById(id);
-		return ResponseEntity.ok(obj);
+		return ResponseEntity.ok(new TecnicoDTO(obj));
 	}
 }
