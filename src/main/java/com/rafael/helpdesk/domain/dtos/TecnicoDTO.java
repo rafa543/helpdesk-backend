2452c4fce04c31589d2981ae476d67236b1c7c2b
Rafael Araujo <rafa123839@gmail.com>
2022-09-25 08:06:58 -0300

    validacao nos campos de criacao de novo usuario

--- src/main/java/com/rafael/helpdesk/domain/dtos/TecnicoDTO.java
+++ src/main/java/com/rafael/helpdesk/domain/dtos/TecnicoDTO.java
@@ -6,6 +6,8 @@ import java.util.HashSet;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+import javax.validation.constraints.NotNull;
+
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.rafael.helpdesk.domain.Tecnico;
 import com.rafael.helpdesk.domain.enums.Perfil;
@@ -14,9 +16,13 @@ public class TecnicoDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	protected Integer id;
+	@NotNull(message = "Campo NOME é requerido")
 	protected String nome;
+	@NotNull(message = "Campo CPF é requerido")
 	protected String cpf;
+	@NotNull(message = "Campo EMAIL é requerido")
 	protected String email;
+	@NotNull(message = "Campo SENHA é requerido")
 	protected String senha;
 	protected Set<Integer> perfis = new HashSet<>();
 	
